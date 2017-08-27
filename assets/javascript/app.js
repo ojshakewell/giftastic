99cebac4077a02c01888a08b84fd21b4bb2a0e22
Brian Russell <brianrobertrussell@gmail.com>
2017-08-27 15:30:14 -0600

    working version

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -10,60 +10,87 @@ $(document).ready(function(){
 //create an array of strings, each one related to a topic that interests you. Save it to a variable called topics. 
 var topics = ["Richard Simmons", "Russell Simmons", "Gene Simmons", "JK Simmons", "Jemma Simmons"];
 
-
-
 var displayButton;
 
+//addToArray();
+showButtons();
 //Your app should take the topics in this array and create buttons in your HTML.
 //Try using a loop that appends a button for each string in the array.
-for (var i = 0; i < topics.length; i++) {
+function showButtons(){
+		$("#display-buttons").empty();
+	for (var i = 0; i < topics.length; i++) {
 
-	displayButton = $('<input type="button" class = "button" value="' + topics[i] + '"/><br />');
-	$("#display-buttons").append(displayButton);
-}
+		displayButton = $('<input type="button" class = "button btn btn-primary" value="' + topics[i] + '"/>');
+		$("#display-buttons").append(displayButton);
+	}
+};
 
+// function addToArray(){
+// 		console.log("input");
+// 	};
+
+$(document).on("click","#user-submit", function(event){
+	event.preventDefault();
+	var input = $("#user-input").val();
+	console.log(input+"input");
+	topics.push(input);
+	showButtons();
+});
 
 $(document).on("click", ".button", function(){
 
 	var inputData = $(this).attr("value");
-	console.log(inputData)
+	//console.log(inputData)
     var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + inputData + "&api_key=dc6zaTOxFJmzC&limit=10";
-	console.log(queryURL);
+	//console.log(queryURL);
 //When the user clicks on a button, the page should grab 10 static, non-animated gif images from the GIPHY API and place them on the page. 
 	$.ajax({
 		url: queryURL,
 		method: 'GET'
-	}).done(function(response) {
+	})
+	.done(function(response) {
 		var responseData = response.data;
-		console.log(responseData);
+		//console.log(responseData);
 
-		for (var i = 0; i < 10; i++) {
+		for (var i = 0; i < responseData.length; i++) {
 
 			// create DIV to display gifs
 			//display GIF
-			var newImage = $("<img>");
-			newImage.attr("src", responseData[i].images.fixed_height.url);
 
-			console.log(i);
-
+			var newImage = $("<img class='gif'>");
+			newImage.attr("src", responseData[i].images.fixed_height_still.url);
+			newImage.attr("data-animate", responseData[i].images.fixed_height.url)
+			newImage.attr("data-still", responseData[i].images.fixed_height_still.url);
+			newImage.attr("data-state", "still");
 			//display Rating
 			// Under every gif, display its rating (PG, G, so on). This data is provided by the GIPHY API.
 			var rating = responseData[i].rating;
-
             var ratingTag = $("<p>").text("Rating: " + rating);
-
             var newDiv = $('<div>');
-            newDiv.append(newImage);
-            newDiv.append(ratingTag);
+          
 
-            $("#display-gifs").append(newDiv);
+            newDiv.prepend(newImage);
+            newDiv.prepend(ratingTag);
+
+            $("#display-gifs").prepend(newDiv);
 		}// end for loop
+
 	});//end .done
-//When the user clicks one of the still GIPHY images, the gif should animate. If the user clicks the gif again, it should stop playing.
-
-
 });//end input.click
 });// end .ready
+//When the user clicks one of the still GIPHY images, the gif should animate. If the user clicks the gif again, it should stop playing.
+$(document).on("click",".gif", function(){
+
+		var gifState = $(this).attr("data-state")
+		//if the state is still, update the attribute
+		if (gifState === "still") {
+			$(this).attr("src", $(this).attr("data-animate"));
+			$(this).attr("data-state", "animate");
+		} else {
+			$(this).attr("src", $(this).attr("data-still"));
+			$(this).attr("data-state", "still");
+		};//end if
+	});
 
 
 
